508de21940f8e0c7750901b424147c949321441d
ERadince <d.radaev99@gmaail.com>
2024-08-07 22:18:49 +0300

    delete all

--- currency-exchange/src/main/java/org/example/models/ExchangeRate.java
+++
@@ -1,8 +0,0 @@
-package org.example.models;
-
-public class ExchangeRate {
-    private Integer id;
-    private Currency baseCurrency;
-    private Currency targetCurrency;
-    private Double rate;
-}
--- currency-exchange/src/main/java/org/example/servlets/ExchangeRatesServlet.java
+++
@@ -1,6 +0,0 @@
-package org.example.servlets;
-
-import javax.servlet.http.HttpServlet;
-
-public class ExchangeRatesServlet extends HttpServlet {
-}
